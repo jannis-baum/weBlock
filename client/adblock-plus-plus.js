0e28cbce1991e6b1bbf9bc93ae91d92eec30e059
Jannis Baum <jannis.baum@gmail.com>
2021-11-14 19:01:04 +0100

    sends and receives full page html

--- client/adblock-plus-plus.js
+++ client/adblock-plus-plus.js
@@ -1,11 +1,12 @@
 const xhr = new XMLHttpRequest();
-xhr.open("GET", 'http://saltleague.net:6969');
+xhr.open("POST", 'http://saltleague.net:6969');
 
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
+      console.log('status: ' + xhr.status);
+      console.log('response: ' + xhr.responseText);
+      document.documentElement.innerHTML = xhr.responseText;
+   }
+};
 
-xhr.send();
-
+xhr.send(document.documentElement.innerHTML);
