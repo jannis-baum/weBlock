618dfc97fec5a602cea4d2dcb6c10bb095aec054
Jannis Baum <jannis.baum@gmail.com>
2021-11-17 14:33:48 +0100

    add PageElement, fix synonym bug in NLP

--- server/nlp.py
+++ server/nlp.py
@@ -3,7 +3,7 @@ import nltk
 nltk.download('wordnet')
 from nltk.corpus import wordnet
 
-class ABPP_NLP:
+class NLProcessor:
     __word_vectors = api.load("glove-wiki-gigaword-100")
     __filter_chars = ['.', ',', '?', ';', '"', '#', '\'', '!', '‘', '’', '“', '”', '…', ':']
 
@@ -11,7 +11,7 @@ class ABPP_NLP:
     def __normal_set(words):
         normal = set()
         for word in [word.lower() for word in words]:
-            normal.add(''.join([character for character in word if character not in ABPP_NLP.__filter_chars]))
+            normal.add(''.join([character for character in word if character not in NLProcessor.__filter_chars]))
         return normal
     
     @staticmethod
@@ -20,13 +20,15 @@ class ABPP_NLP:
         for syn in wordnet.synsets(word): 
             for lemm in syn.lemmas(): 
                 syns += [lemm.name(), lemm.name() + 's']
-        return set([syn for syn in ABPP_NLP.__normal_set(syns) if word not in syn] + [word, word + 's'])
+        return set([syn for syn in NLProcessor.__normal_set(syns) if word not in syn] + [word, word + 's'])
 
     @staticmethod
-    def similarity(crucial, statement, compare):
-        crucial_syns = [ABPP_NLP.__synonyms(cruc) for cruc in crucial]
-        compare_normal_set = ABPP_NLP.__normal_set(compare.split(' '))
-        if not crucial and not crucial_syns & compare_normal_set:
+    def similarity(requirements, statement, compare):
+        if not requirements:
             return float('inf')
-        return ABPP_NLP.__word_vectors.wmdistance(compare, statement)
+        req_syns = set.union(*[NLProcessor.__synonyms(req) for req in requirements])
+        compare_normal_set = NLProcessor.__normal_set(compare.split(' '))
+        if not (req_syns & compare_normal_set):
+            return float('inf')
+        return NLProcessor.__word_vectors.wmdistance(compare, statement)
 
--- server/page_processor.py
+++ server/page_processor.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup
+from nlp import NLProcessor
+
+class PageElement:
+    def __init__(self, text, censoring_requirements, censoring_statement):
+        self.text = text
+        self.similarity = NLProcessor.similarity(censoring_requirements, censoring_statement, self.text)
+    
+    def string(self):
+        return f'    text:\n        {self.text}\n    similarity: {self.similarity}'
 
 class PageProcessor:
     relevant_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'p']
 
-    def __init__(self, html):
+    def __init__(self, censoring_statement, censoring_requirements, html):
         self.soup = BeautifulSoup(html, features='html.parser')
-        self.paragraphs = [paragraph.text for paragraph in self.soup.find_all(PageProcessor.relevant_tags)]
-        self.paragraphs = dict(zip(PageProcessor.relevant_tags, [
-            [paragraph.text for paragraph in self.soup.find_all(key_tag)] for key_tag in PageProcessor.relevant_tags
-        ]))
+        self.paragraphs = dict(zip(PageProcessor.relevant_tags,
+            [
+                [PageElement(element.text, censoring_requirements, censoring_statement) for element in self.soup.find_all(key_tag)]
+            for key_tag in PageProcessor.relevant_tags]
+        ))
 
