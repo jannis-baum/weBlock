6eae08d3514c0fce4cb202e4f9ff95abcd0a76fa
Jannis Baum <jannis.baum@gmail.com>
2021-11-21 19:43:07 +0100

    client modifies page according to json

--- client/adblock-plus-plus.js
+++ client/adblock-plus-plus.js
@@ -1,11 +1,25 @@
 const xhr = new XMLHttpRequest();
 xhr.open("POST", 'http://saltleague.net:6969');
 
+function edit_page(data) {
+   const key_tag = 'tag';
+   const key_edits = 'edits';
+   const key_index = 'index';
+   const key_innerHTML = 'innerHTML';
+
+   for (let dict of data) {
+      elements = document.getElementsByTagName(dict[key_tag]);
+      for (edit of dict[key_edits]) {
+         elements[edit[key_index]].innerHTML = edit[key_innerHTML];
+      }
+   }
+}
+
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log('status: ' + xhr.status);
       console.log('response: ' + xhr.responseText);
-      document.documentElement.innerHTML = xhr.responseText;
+      data = JSON.parse(xhr.responseText);
    }
 };
 
