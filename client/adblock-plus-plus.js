903e5b2bc99b31015bbc4d28007183253fe68c59
Jannis Baum <jannis.baum@gmail.com>
2021-11-22 20:27:10 +0100

    client edits page and hides page while waiting for server response

--- client/adblock-plus-plus.js
+++ client/adblock-plus-plus.js
@@ -1,6 +1,3 @@
-const xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://saltleague.net:6969');
-
 function edit_page(data) {
    const key_tag = 'tag';
    const key_edits = 'edits';
@@ -15,12 +12,18 @@ function edit_page(data) {
    }
 }
 
+document.documentElement.hidden = true;
+
+const xhr = new XMLHttpRequest();
+xhr.open("POST", 'http://saltleague.net:6969');
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
       console.log('status: ' + xhr.status);
       console.log('response: ' + xhr.responseText);
       data = JSON.parse(xhr.responseText);
+      edit_page(data);
+      document.documentElement.hidden = false;
    }
 };
-
 xhr.send(document.documentElement.innerHTML);
+
