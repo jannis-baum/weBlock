ce0002ea2e29779fa2bef1d3b9830266c332f6f4
Jannis Baum <jannis.baum@gmail.com>
2021-11-30 22:20:33 +0100

    client remove js debugging prints

--- client/adblock-plus-plus.js
+++ client/adblock-plus-plus.js
@@ -14,14 +14,9 @@ function sendstring() {
 }
 
 function edit_page(data) {
-   console.log('func called');
    for (let tag in data) {
-      console.log(tag);
       elements = document.getElementsByTagName(tag);
-      console.log('extracted elements');
-      console.log(elements);
       for (let n = 0; n < elements.length; n++) {
-         console.log(n);
          elements[n].innerHTML = data[tag][n];
       }
    }
@@ -33,7 +28,6 @@ const xhr = new XMLHttpRequest();
 xhr.open("POST", 'http://saltleague.net:6969');
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
-      console.log(xhr.responseText);
       data = JSON.parse(xhr.responseText);
       edit_page(data);
       document.documentElement.hidden = false;
