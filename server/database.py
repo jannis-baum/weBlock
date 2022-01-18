4a623c8fbd25ae5f137d045fd4e72afdbd4bf62e
Jannis Baum <jannis.baum@gmail.com>
2022-01-18 11:03:52 +0100

    database insertion statement

--- server/database.py
+++ server/database.py
@@ -3,15 +3,19 @@ from dotenv import load_dotenv; load_dotenv()
 import mysql.connector
 
 class Database:
+    __insertion = 'insert into summaries (source, summary, topic) values (%s, %s, %s)'
     __db = mysql.connector.connect(
-        host=os.getenv('HOST'),
-        user=os.getenv('USER'),
-        password=os.getenv('PASSWORD'),
-        database=os.getenv('DATABASE'))
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_DATABASE'))
     __cursor = __db.cursor()
 
     @staticmethod
-    def execute(sql):
-        Database.__cursor.execute(sql)
+    def add_values(vals):
+        if type(vals) is list:
+            Database.__cursor.executemany(Database.__insertion, vals)
+        else:
+            Database.__cursor.execute(Database.__insertion, vals)
         Database.__db.commit()
 
