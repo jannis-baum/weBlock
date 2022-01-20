a8fc33349c060e2c81e5fe78d8d6da8c040b0381
Lucas Liebe <lucasliebe.ll@gmail.com>
2022-01-20 22:30:44 +0100

    add scraper page processor and integration

--- scraper/main.py
+++ scraper/main.py
@@ -1,11 +1,19 @@
 import os
+import urllib.request
 from dotenv import load_dotenv
-
+from page_processor import PageProcessor
 from article_scraper import GoogleScraper
+from database import Database
+from nlp import NLProcessor
 
 if __name__ == '__main__':
     load_dotenv()
-    urls = GoogleScraper.find_urls_for_query(os.getenv('SEARCH_QUERY'))
+    topic = os.getenv('SEARCH_QUERY')
+    urls = GoogleScraper.find_urls_for_query(topic)
     GoogleScraper.quit()
-    print(urls)
-
+    for url in urls:
+        page = urllib.request.urlopen(url)
+        processor = PageProcessor(page.read().decode("utf8"))
+        page.close()
+        summarization = NLProcessor.save_summarized(processor.get_fulltext())
+        Database.insert([url, summarization, topic])
---
+++ scraper/nlp.py
@@ -0,0 +1,35 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+
+class NLProcessor:
+    __stop_words_path = 'stop_words_en.txt'
+    __stop_words = None
+    __sim_statements = list()
+
+    @staticmethod
+    def __get_stop_words():
+        if not NLProcessor.__stop_words:
+            with open(NLProcessor.__stop_words_path, 'r') as fp:
+                NLProcessor.__stop_words = fp.read().split('\n')
+        return NLProcessor.__stop_words
+
+    @staticmethod
+    def __normalize(text):
+        return ' '.join([word.lower() for word in text.split(' ') if word not in NLProcessor.__get_stop_words()])
+
+    @staticmethod
+    def save_summarized(document):
+        sentences = sent_tokenize(document)
+        tokens = list({ token for sentence in sentences for token in word_tokenize(sentence) if token not in NLProcessor.__get_stop_words() })
+
+        word_frequencies = [ (token, document.count(token)) for token in tokens ]
+        word_frequencies.sort(key=lambda x: x[1])
+        max_frequency = word_frequencies[-1][1]
+
+        weighted_frequencies = { freq[0]: freq[1] / max_frequency for freq in word_frequencies }
+
+        sentence_scores = [
+            (sentence, sum([ weighted_frequencies[word] for word in sentence.split(' ') if word in tokens ]))
+        for sentence in sentences]
+        sentence_scores.sort(reverse=True, key=lambda x: x[1])
+        NLProcessor.__sim_statements.append(NLProcessor.__normalize(sentence_scores[:max(2, int(len(sentences) / 20))]))
