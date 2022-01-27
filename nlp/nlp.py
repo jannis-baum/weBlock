303401c81a78232f7efe1dfee567f532b6550926
Jannis Baum <jannis.baum@gmail.com>
2022-01-27 11:04:41 +0100

    fix paths

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,4 +1,3 @@
-from os import fpathconf
 import gensim.downloader as api
 import nltk
 nltk.download('wordnet'); nltk.download('vader_lexicon'); nltk.download('punkt'); nltk.download('stopwords')
--- scraper/main.py
+++ scraper/main.py
@@ -1,12 +1,10 @@
 import os
-import sys
-sys.path.append('../nlp')
 import requests
 from dotenv import load_dotenv
 from page_processor import PageProcessor
 from article_scraper import GoogleScraper
-#from database import Database
-from nlp import NLProcessor
+from database.database import Database
+from nlp.nlp import NLProcessor
 
 if __name__ == '__main__':
     load_dotenv()
@@ -18,5 +16,5 @@ if __name__ == '__main__':
         processor = PageProcessor(page)
         summarization = ''.join(NLProcessor.summarize(processor.get_fulltext()))
         print(summarization)
-        #Database.insert([url, summarization, topic])
+        Database.insert([url, summarization, topic])
 
