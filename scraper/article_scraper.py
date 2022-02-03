edfd5c871a9822b029612bbafb0e29870a2c8555
Jannis Baum <jannis.baum@gmail.com>
2022-02-03 14:35:02 +0100

    add google news scraping

--- scraper/article_scraper.py
+++ scraper/article_scraper.py
@@ -1,4 +1,3 @@
-from argparse import Namespace
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -7,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 class GoogleScraper:
-    __opts = Options(); __opts.headless = True
+    __opts = Options();# __opts.headless = True
     __driver = None
 
     @staticmethod
@@ -39,4 +38,37 @@ class GoogleScraper:
             results = fetch_results()
             time.sleep(1)
         return results
+
+    def find_news_htlms_for_query(query):
+        GoogleScraper.__get_driver().get('https://news.google.com')
+        try:
+            agree_btn = next(btn for btn in GoogleScraper.__get_driver().find_elements(By.TAG_NAME, 'button') if 'I agree' in btn.get_attribute('innerHTML'))
+            agree_btn.click()
+        except: pass
+
+        search_bar = GoogleScraper.__get_driver().find_element(By.XPATH, '//input[@value="Search for topics, locations & sources"]')
+
+        action = webdriver.common.action_chains.ActionChains(GoogleScraper.__get_driver())
+        action.move_to_element_with_offset(search_bar, 5, 5)
+        action.click()
+        action.send_keys("China censors content" + Keys.ENTER)
+        action.perform()
+
+        time.sleep(3)
+        fetch_results = lambda: [a_tag.get_attribute('href') for a_tag in GoogleScraper.__get_driver().find_elements(By.XPATH, '//article[@ve-visible="true"]/a')][:10]
+        results = list()
+        while not results:
+            results = fetch_results()
+        results_prev = list()
+        while len(results_prev) < len(results):
+            results_prev = results
+            results = fetch_results()
+            time.sleep(1)
+
+        htmls = list()
+        for url in results:
+            GoogleScraper.__get_driver().get(url)
+            time.sleep(3)
+            htmls.append(GoogleScraper.__get_driver().page_source)
+        return htmls
 
