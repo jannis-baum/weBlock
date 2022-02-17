061f407ef4e9de56201dff593ef5b720cbe933aa
Kali Live user <Tilman Schtze>
2022-02-17 14:52:45 +0000

    starting to test on a normal query

--- server/definitions.py
+++ server/definitions.py
@@ -1,7 +1,8 @@
 import os
-from dotenv import load_dotenv; load_dotenv()
+from dotenv import load_dotenv
+
+load_dotenv(".env.example")
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-SEARCH_QUERY = os.getenv('SEARCH_QUERY')
-
+SEARCH_QUERY = os.getenv("SEARCH_QUERY")
