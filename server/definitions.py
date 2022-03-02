b5f4a141b37c2170f40bda458b9a92c222c6ad71
tiny-fish-T <Tilman Schtze>
2022-03-02 13:33:20 +0000

    fixed faulty database insertion & finished positive sentence feature

--- server/definitions.py
+++ server/definitions.py
@@ -6,4 +6,4 @@ load_dotenv()
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 SEARCH_QUERY = os.getenv("SEARCH_QUERY")
-POSITIVE_QUERIES = os.getenv("POSITIVE_QUERIES")
+POSITIVE_QUERIES = list(eval(os.getenv("POSITIVE_QUERIES")))
