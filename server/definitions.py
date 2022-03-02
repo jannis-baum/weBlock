c74d84b20d837dffddc1b0fb7ba9542b21547d75
Jannis Baum <jannis.baum@gmail.com>
2022-03-02 15:24:04 +0100

    add training parameters to env

--- server/definitions.py
+++ server/definitions.py
@@ -7,3 +7,10 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 SEARCH_QUERY = os.getenv("SEARCH_QUERY")
 POSITIVE_QUERIES = os.getenv("POSITIVE_QUERIES").split(", ")
+
+TRAINING_TOPICS = int(os.getenv("TRAINING_TOPICS", "20"))
+TRAINING_ALPHA = float(os.getenv("TRAINING_ALPHA", "2.5"))
+TRAINING_BETA = float(os.getenv("TRAINING_BETA", "0.05"))
+TRAINING_ITERATIONS = int(os.getenv("TRAINING_ITERATIONS", "10"))
+TRAINING_SAVE_STEPS= int(os.getenv("TRAINING_SAVE_STEPS", "500"))
+
