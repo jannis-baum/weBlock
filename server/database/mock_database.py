2db2bd433c7ea1c26588bd74edb0c60ad0877906
tiny-fish-T <Tilman Schtze>
2022-03-02 13:22:48 +0000

    added new database tables & classes for positive sentences

--- server/database/mock_database.py
+++ server/database/mock_database.py
@@ -1,13 +1,14 @@
 from definitions import ROOT_DIR
 import yaml, os
 
+
 class Database:
-    __mock_database_name = os.path.join(ROOT_DIR, 'database/.mock-database.yaml')
+    __mock_database_name = os.path.join(ROOT_DIR, "database/.mock-database.yaml")
 
     @staticmethod
     def __get_data():
         try:
-            with open(Database.__mock_database_name, 'r') as mock_db:
+            with open(Database.__mock_database_name, "r") as mock_db:
                 return yaml.safe_load(mock_db.read())
         except:
             return list()
@@ -15,11 +16,35 @@ class Database:
     @staticmethod
     def insert(vals):
         data = Database.__get_data()
-        data.append(dict(zip(['source', 'text', 'topic'], vals)))
-        with open(Database.__mock_database_name, 'w') as mock_db:
+        data.append(dict(zip(["source", "text", "topic"], vals)))
+        with open(Database.__mock_database_name, "w") as mock_db:
             yaml.dump(data, mock_db, default_flow_style=False)
-    
+
     @staticmethod
     def get_summaries():
-        return [row['text'] for row in Database.__get_data()]
+        return [row["text"] for row in Database.__get_data()]
 
+
+class DatabasePositive:
+    __mock_database_name = os.path.join(
+        ROOT_DIR, "database/.mock-database-positive.yaml"
+    )
+
+    @staticmethod
+    def __get_data():
+        try:
+            with open(DatabasePositive.__mock_database_name, "r") as mock_db:
+                return yaml.safe_load(mock_db.read())
+        except:
+            return list()
+
+    @staticmethod
+    def insert(value):
+        data = DatabasePositive.__get_data()
+        data.append(dict(zip(["paragraph"], value)))
+        with open(DatabasePositive.__mock_database_name, "w") as mock_db:
+            yaml.dump(data, mock_db, default_flow_style=False)
+
+    @staticmethod
+    def get_sentences():
+        return [row["paragraph"] for row in DatabasePositive.__get_data()]
--- server/scraper/page_processor.py
+++ server/scraper/page_processor.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 
 class PageProcessor:
     def __init__(self, html):
-        self.__soup = BeautifulSoup(html, features='html.parser')
+        self.__soup = BeautifulSoup(html, features="html.parser")
 
     def get_fulltext(self):
-        return ' '.join([element.text for element in self.__soup.find_all('p')])
+        return " ".join([element.text for element in self.__soup.find_all("p")])
 
+    def get_all_paragraphs(self):
+        return [element.text for element in self.__soup.find_all("p")]
