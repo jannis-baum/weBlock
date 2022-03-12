f0427f53fc7ad4e81523adeac66956366fdf51f8
Jannis Baum <jannis.baum@gmail.com>
2022-03-12 14:46:21 +0100

    introduce text_matcher to replace text_generator

---
+++ server/backend/text_matcher.py
@@ -0,0 +1,18 @@
+import numpy as np
+import yaml
+
+from definitions import TOPIC_MODEL_PATH
+from database.mock_database import DatabasePositive
+from btm.script.infer import BTMInferrer
+
+class TextMatcher:
+    def __init__(self):
+        self.__inferrer = BTMInferrer(TOPIC_MODEL_PATH)
+        self.__topic_matrix = np.array(DatabasePositive.get_topic_matrix())
+        self.__paragraphs = DatabasePositive.get_sentences()
+
+    def find_matching(self, document):
+        topic_vector = np.array(self.__inferrer.infer(document))
+        idx = np.argmax(self.__topic_matrix.dot(topic_vector))
+        return self.__paragraphs[idx]
+
